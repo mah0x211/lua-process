552cdf8fb1d31af3531cdfe4af236312680b9493
Masatoshi Teruya <mah0x211@gmail.com>
2014-12-27 15:50:41 +0900

    change: gid_try.lua has been updated.

--- test/gid_try.lua
+++ test/gid_try.lua
@@ -1,13 +1,25 @@
 local getgid = require('process').getgid;
 local setgid = require('process').setgid;
 local setegid = require('process').setegid;
+local setregid = require('process').setregid;
 local gid;
 
 ifNil( getgid() );
 gid = ifNil( getgid('staff') );
+ifNotNil( getgid('invalid gid') );
+
 ifNotNil( setgid( gid ) );
 ifNotNil( setegid( gid ) );
+
 ifNotNil( setgid('staff') );
+ifNil( setgid('invalid gid') );
+
 ifNotNil( setegid('staff') );
+ifNil( setegid('invalid gid') );
+
+ifNotNil( setregid( gid, gid ) );
+ifNotNil( setregid( 'staff', 'staff' ) );
+ifNil( setregid( 'invalid gid', 'invalid gid' ) );
+
 ifNotEqual( gid, getgid() );
 
