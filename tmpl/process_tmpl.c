a43c55f7183064c386d18f97aa76ddba1cb5e227
Masatoshi Teruya <mah0x211@gmail.com>
2014-12-27 15:02:06 +0900

    change: argument type of setgid function has been changed.

--- tmpl/process_tmpl.c
+++ tmpl/process_tmpl.c
@@ -102,10 +102,28 @@ static int getgid_lua( lua_State *L )
 
 static int setgid_lua( lua_State *L )
 {
-    gid_t gid = (gid_t)luaL_checkinteger( L, 1 );
-    
-    if( setgid( gid ) == 0 ){
-        return 0;
+    if( lua_type( L, 1 ) == LUA_TSTRING )
+    {
+        const char *gname = luaL_checkstring( L, 1 );
+        struct group *grp = NULL;
+        
+        errno = 0;
+        // set gid by group-name
+        if( ( grp = getgrnam( gname ) ) && setgid( grp->gr_gid ) == 0 ){
+            return 0;
+        }
+        // group not found
+        else if( !errno ){
+            errno = EINVAL;
+        }
+    }
+    else
+    {
+        gid_t gid = (gid_t)luaL_checkinteger( L, 1 );
+        
+        if( setgid( gid ) == 0 ){
+            return 0;
+        }
     }
     
     // got error
