e251c20024c3bfd4ef1138feb21878d2d0887d06
Masatoshi Teruya <mah0x211@gmail.com>
2014-12-29 00:11:43 +0900

    add: test/uid_try.lua has been added.

---
+++ test/uid_try.lua
@@ -0,0 +1,27 @@
+local getuid = require('process').getuid;
+local getuname = require('process').getuname;
+local setuid = require('process').setuid;
+local seteuid = require('process').seteuid;
+local setreuid = require('process').setreuid;
+local uid, uname;
+
+uid = ifNil( getuid() );
+uname = ifNil( getuname( uid ) );
+ifNotEqual( uid, getuid( uname ) );
+ifNotNil( getuid('invalid uid') );
+
+ifNotNil( setuid( uid ) );
+ifNotNil( seteuid( uid ) );
+
+ifNotNil( setuid( uname ) );
+ifNil( setuid('invalid uid') );
+
+ifNotNil( seteuid( uname ) );
+ifNil( seteuid('invalid uid') );
+
+ifNotNil( setreuid( uid, uid ) );
+ifNotNil( setreuid( uname, uname ) );
+ifNil( setreuid( 'invalid uid', 'invalid uid' ) );
+
+ifNotEqual( uid, getuid() );
+
